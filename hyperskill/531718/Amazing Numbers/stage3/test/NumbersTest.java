debe4f7d2952273211f8d7c856738a2bc2661952
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-05-16 11:39:45 +0300

    #jf8qf7[in progress] Add hyperskill project

---
+++ hyperskill/531718/Amazing Numbers/stage3/test/NumbersTest.java
@@ -0,0 +1,110 @@
+import org.hyperskill.hstest.dynamic.DynamicTest;
+import org.hyperskill.hstest.stage.StageTest;
+import org.hyperskill.hstest.testcase.CheckResult;
+import util.Checker;
+import util.RegexChecker;
+import util.TextChecker;
+import util.UserProgram;
+
+import java.util.Random;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.LongStream;
+
+public final class NumbersTest extends StageTest {
+    private static final Random random = new Random();
+
+    private static final int NEGATIVE_NUMBERS_TESTS = 5;
+    private static final int FIRST_NUMBERS = 15;
+    private static final int RANDOM_TESTS = 10;
+
+    private static final Checker WELCOME = new TextChecker("Welcome to Amazing Numbers!");
+
+    private static final String EXPLAIN = "The program should explain this in the help.";
+    private static final Function<UserProgram, UserProgram> HELP =
+            new TextChecker("Supported requests")
+                    .andThen(new TextChecker(
+                            "enter a natural number",
+                            "In this stage, a user can enter one number to print a card. " + EXPLAIN))
+                    .andThen(new TextChecker(
+                            "enter 0 to exit",
+                            "Display the instructions on how to exit"));
+
+    private static final Checker ASK_REQUEST = new TextChecker(
+            "enter a request",
+            "The program should ask a user to enter a request."
+    );
+    private static final Checker ERROR_FIRST = new RegexChecker(
+            "The first (parameter|number) should be a natural number or zero",
+            "The first parameter \"{0}\" is wrong. The program should print an error message."
+    );
+    private static final Checker PROPERTIES_OF = new RegexChecker(
+            "properties of \\d",
+            "The first line of number's properties should contain \"Properties of {0}\"."
+    );
+    private static final Checker RUNNING = new Checker(Predicate.not(UserProgram::isFinished),
+            "The program should continue to work till the user enter \"0\"."
+    );
+    private static final Checker FINISHED = new Checker(UserProgram::isFinished,
+            "The program should finish when the user entered \"0\"."
+    );
+    private final UserProgram program = new UserProgram();
+
+    // Stage #3
+
+    @DynamicTest(order = 5)
+    CheckResult welcomeTest() {
+        return program
+                .start()
+                .check(WELCOME)
+                .check(HELP)
+                .check(RUNNING)
+                .check(ASK_REQUEST)
+                .execute(0)
+                .check(FINISHED)
+                .result();
+    }
+
+    @DynamicTest(repeat = NEGATIVE_NUMBERS_TESTS, order = 10)
+    CheckResult notNaturalNumbersTest() {
+        long negativeNumber = -random.nextInt(Byte.MAX_VALUE) - 1L;
+        return program
+                .start()
+                .check(WELCOME)
+                .check(HELP)
+                .check(ASK_REQUEST)
+                .execute(negativeNumber)
+                .check(ERROR_FIRST)
+                .check(RUNNING)
+                .check(ASK_REQUEST)
+                .execute(0)
+                .check(FINISHED)
+                .result();
+    }
+
+    @DynamicTest(order = 20)
+    CheckResult naturalNumbersTest() {
+        final var numbers = LongStream.concat(
+                LongStream.range(1, FIRST_NUMBERS),
+                random.longs(RANDOM_TESTS, 1, Long.MAX_VALUE)
+        );
+
+        program.start().check(WELCOME).check(HELP);
+
+        numbers.forEach(number -> program
+                .check(ASK_REQUEST)
+                .execute(number)
+                .check(PROPERTIES_OF)
+                .check(new PropertiesChecker(number))
+                .check(RUNNING));
+
+        return program
+                .check(RUNNING)
+                .check(ASK_REQUEST)
+                .execute(0)
+                .check(FINISHED)
+                .result();
+    }
+
+
+}
