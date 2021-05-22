5856d06f4d9e3048c1fff6b213b20ad7fdad7102
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-05-22 16:06:53 +0300

    #12 Fixed error in feedback

--- hyperskill/531718/Amazing Numbers/stage1/test/BuzzChecker.java
+++ hyperskill/531718/Amazing Numbers/stage1/test/BuzzChecker.java
@@ -1,14 +1,15 @@
 import util.Checker;
 import util.UserProgram;
 
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 public class BuzzChecker extends Checker {
     private static final Pattern BUZZ_NUMBER = Pattern.compile(
             "is(?<buzz>n't| not)?+( a)?+ buzz", Pattern.CASE_INSENSITIVE);
     private static final Pattern EXPLANATION = Pattern.compile(
-            "divisible by 7 and it ends with 7|divisible by 7|ends with 7"
-                    + "|neither divisible by 7 nor it ends with 7", Pattern.CASE_INSENSITIVE);
+            "divisible by (7|seven) and it ends with (7|seven)|divisible by (7|seven)|ends with (7|seven)"
+                    + "|neither divisible by (7|seven) nor it ends with (7|seven)", Pattern.CASE_INSENSITIVE);
 
     private final long number;
 
@@ -29,8 +30,10 @@ public class BuzzChecker extends Checker {
         final var actual = matcher.group("buzz") == null;
 
         if (actual != isBuzz) {
+            final var expectedMessage = isBuzz ? "" : " not";
+            final var actualMessage = Objects.toString(matcher.group("buzz"), "");
             feedback = "For {0} expected: \"is{1} a Buzz number\". Actual output: \"is{2} a Buzz number.";
-            parameters = new Object[]{number, isBuzz ? "" : " not", matcher.group("buzz")};
+            parameters = new Object[]{number, expectedMessage, actualMessage};
             return false;
         }
 
